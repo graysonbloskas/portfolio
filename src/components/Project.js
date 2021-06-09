7179606618857c060a5ce245732bfb1579191ef5
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-08 20:47:42 -0400

    finished the projects page, project details are rendering beautifully

--- src/components/Project.js
+++ src/components/Project.js
@@ -23,6 +23,7 @@ export default function Project() {
                 <h1 className="text-5xl flex justify-center">My Projects</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my Projects Page!</h2>
                 <section className="grid grid-cols-2 gap-8">
+                    {projectData && projectData.map((project, index) => (
                     <article className="relative rounded-lg shadow-xl bg-white p-16">
                         <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
                             <a
@@ -37,12 +38,22 @@ export default function Project() {
                                 <strong className="font-bold">Finished On</strong>:{" "}
                                 {new Date(project.date).toLocaleDateString()}
                             </span>
-                            <span></span>
-                            <span></span>
-                            <p></p>
-                            <span></span>
+                            <span>
+                                <strong className="font-bold">Company</strong>:{" "}
+                                {project.place}
+                            </span>
+                            <span>
+                                <strong className="font-bold">Type</strong>:{" "}
+                                {project.projectType}
+                            </span>
+                            <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}</p>
+                            <a href={project.link} rel="noopener noreferrer" target="_blank" className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl">
+                                View the Project{" "}
+                            <span role="img" aria-label="right pointer">👉</span>
+                            </a>
                         </div>
                     </article>
+                    ))}
                 </section>
             </section>
         </main>
