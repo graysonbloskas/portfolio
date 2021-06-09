396a6fba4f46b36e06c9b6abacf73a5f862990d8
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-08 22:42:10 -0400

    finished about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import sanityClient from '../client.js';
+import background from '../new-york-city-manhattan-skyline-black-and-white-ae-PJKA3SD.jpg';
+import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from '@sanity/block-content-to-react';
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source);
+};
 
 export default function About() {
-    return <h1>About Page!</h1>
+    const [author, setAuthors] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == 'author']{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`).then((data) => setAuthors(data[0]))
+        .catch(console.error)
+    }, [])
+
+    if(!author) return <div>Loading..</div>
+
+    return (
+        <main className="relative">
+            <img src={background} alt="city skyline" className="absolute w-full" />
+            <div className="p-10 lg:pt-48 container mx-auto relative">
+                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 h-64 mr-8" alt={author.name} />
+                    <div className="text-lg flex flex-col justify-center">
+                        <h1 className="cursive text-6xl text-green-300 mb-4">Hi! I'm{' '}
+                        <span className="text-green-100">{author.name}</span>
+                        </h1>
+                        <div className="prose lg:prose-xl text-white">
+                            <BlockContent blocks={author.bio} projectId="m80s6cjc" dataset="production" />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
 }
